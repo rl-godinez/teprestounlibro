c261b9f219b30fd30a4210cd4f60b15b8f52f4d6
Lenin Godinez <lenin@michelada.io>
2019-08-27 15:25:07 +0000

    Add devise admin user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
+  devise_for :admins
+
   resources :categories
 end
