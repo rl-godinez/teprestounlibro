e56c7689eaf04d44911924e284568d41d30d80dc
Lenin Godinez <lenin@michelada.io>
2019-09-05 18:03:16 +0000

    Fix admin categories custom actions

--- app/dashboards/category_dashboard.rb
+++ app/dashboards/category_dashboard.rb
@@ -24,16 +24,14 @@ class CategoryDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    book_categories
-    books
     id
+    books
     name
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-    book_categories
     books
     id
     name
@@ -45,8 +43,6 @@ class CategoryDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    book_categories
-    books
     name
   ].freeze
 
