756f2a4fa11c55fce3fd0ea5243b8684edea492f
Melissa Juarez <melissa.juarez@michelada.io>
2019-09-06 18:11:52 -0500

    add nav bar and flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
+  add_flash_types(:success, :info, :warning, :danger)
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
-
+#
 module ApplicationHelper
+  def flash_helper(name)
+    case name
+    when 'notice'
+      'alert-success'
+    when 'alert'
+      'alert-danger'
+    end
+  end
 end
+
