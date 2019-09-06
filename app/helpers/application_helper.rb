de954427c070ee19044462fbd3577e16418a57f1
Melissa Juarez <melissa.juarez@michelada.io>
2019-09-06 18:19:48 -0500

    fix comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 #
 module ApplicationHelper
-  def flash_helper(name)
-    case name
-    when 'notice'
-      'alert-success'
-    when 'alert'
-      'alert-danger'
-    end
-  end
 end
-
